568e8d133562ff4e3ff230f7f30ed8dfd90e55f1
20231126 <aksmfdyfl111@gmail.com>
2023-12-29 18:58:13 +0900

    main

---
+++ googleExample/src/test/java/com/example/googleexample/GoogleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
